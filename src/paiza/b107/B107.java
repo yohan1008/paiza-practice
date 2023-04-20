095a695919134eff497ab7510d4c9490a419e5be
Yohan Kang <kan.yohan@cipher-net.co.jp>
2023-04-20 17:38:17 +0900

    fourth commit

--- src/paiza/b107/B107.java
+++ src/paiza/b107/B107.java
@@ -1,56 +1,82 @@
 package paiza.b107;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
+/**
+ * B107は与えられたカードを決められたカードの枚数ごとにグループ化を行い、グループ単位でシャッフルをした後の結果を出力する問題となる。
+ * ArrayListにグループを配列として保持することでグループ化を行い、シャッフルは単純にクループの順が反転するだけなので、ArrayListのreverseを行う。
+ * 最初にグループ化をした後、その状態で持続的にシャッフルを行うことは意味がないため、シャッフル後に配列に変換後、再度グループ化を行う必用がある。
+ * そのためには、各グループの枚数の情報を保持して置く必用があり、ArrayListに保持をすることにする。
+ * 結論的に「グループ化 → シャッフル → 配列に変換 → グループ化 → シャッフル → 配列に変換」を繰り返すことになる。
+ *
+ * @author kang yohan
+ */
 public class B107 {
-
-    static int cardNum;
+    //カード
     static int[] cardArr;
-    static int groupNum;
+    //シャッフルの回数
     static int shuffleCnt;
+    //グループの枚数
     static ArrayList<Integer> groupNumList = new ArrayList<>();
+    //グループ化されたカード
     static ArrayList<int[]> groupedCardList = new ArrayList<>();
 
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
-            cardNum = sc.nextInt();
-            groupNum = sc.nextInt();
+            //カードの枚数を取得
+            int cardNum = sc.nextInt();
+            //グループの枚数を取得
+            int groupCardNum = sc.nextInt();
+            //シャッフルの回数を取得
             shuffleCnt = sc.nextInt();
+            //カードを取得
             cardArr = new int[cardNum];
-
-            for(int i = 0 ; i < cardArr.length ; i++) {
+            for (int i = 0; i < cardArr.length; i++) {
                 cardArr[i] = i + 1;
             }
 
-            int mok = cardNum / groupNum;
-            int namoge = cardNum % groupNum;
+            //グループ数と余ったカードの枚数を計算
+            //カード / 各グループの枚数　= 必ず存在するグループ数
+            int mainGroupNum = cardNum / groupCardNum;
+            //カード % 各グループの枚数　= グループ化をした後、残りのカード枚数
+            int remainGroupCardNum = cardNum % groupCardNum;
 
-            for (int i = 0; i < mok; i++) {
-                groupNumList.add(groupNum);
+            //各グループの枚数を格納
+            for (int i = 0; i < mainGroupNum; i++) {
+                groupNumList.add(groupCardNum);
             }
 
-            if (namoge != 0) {
-                groupNumList.add(namoge);
+            if (remainGroupCardNum != 0) {
+                groupNumList.add(remainGroupCardNum);
             }
         }
 
         for (int i = 0; i < shuffleCnt; i++) {
+            //グループ化をする
             makeGroup();
+            //シャッフルをする
             shuffle();
         }
 
-        for (int i = 0; i < cardArr.length; i++) {
-            System.out.println(cardArr[i]);
+        for (int card : cardArr) {
+            //最終的にシャッフルが終わったカードを出力する
+            System.out.println(card);
         }
 
     }
 
+    /**
+     * グループ化を行う。
+     */
     static void makeGroup() {
+        //カードのインデックス
         int cardIdx = 0;
-        for (int cnt : groupNumList) {
-            int[] tmpCardArr = new int[cnt];
-            for (int i = 0; i < cnt; i++) {
+        //各グループの枚数通りに配列を作り格納する
+        for (int num : groupNumList) {
+            int[] tmpCardArr = new int[num];
+            for (int i = 0; i < num; i++) {
                 tmpCardArr[i] = cardArr[cardIdx];
                 cardIdx++;
             }
@@ -58,19 +84,21 @@ public class B107 {
         }
     }
 
+    /**
+     * シャッフルを行う
+     */
     static void shuffle() {
-        ArrayList<int[]> tmpGroupedCardList = new ArrayList<>();
-        for(int i = groupedCardList.size() - 1; i >= 0 ; i --) {
-            tmpGroupedCardList.add(groupedCardList.get(i));
-        }
-        groupedCardList = tmpGroupedCardList;
+        //グループ前後の反転を行う
+        Collections.reverse(groupedCardList);
         int cardIdx = 0;
-        for(int[] card : groupedCardList) {
-            for (int i = 0; i < card.length; i++) {
-                cardArr[cardIdx] = card[i];
+        //グループ化されたカードのリストを再度配列に戻す
+        for (int[] tmpCardArr : groupedCardList) {
+            for (int cardNo : tmpCardArr) {
+                cardArr[cardIdx] = cardNo;
                 cardIdx++;
             }
         }
+        //次のグループ化されたカードを保持するためにクリアさせる
         groupedCardList.clear();
     }
 }
